58c175939aa0bdc639df73b4070e91451853d911
marcosjunior00 <marcosantonio.jose01@gmail.com>
2025-05-13 11:50:17 -0300

    feat: preparing Auth Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { AuthProvider } from "./contexts/AuthContext"
 import { GlobalStyles } from "./styles/global-styles"
 import AppRoutes from "./routes"
 
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <GlobalStyles />
       <AppRoutes />
-    </>
+    </AuthProvider>
   )
 }
 
