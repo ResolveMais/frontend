0a77a4bb48ce87b5a9c2dc1fb53d71c9a8bf1195
marcosjunior00 <marcosantonio.jose01@gmail.com>
2025-05-24 19:01:28 -0300

    fix: button component adjusts

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
 import * as S from "./styles"
 import PropTypes from 'prop-types'
 
-const Button = ({ variant, children }) => {
-  return (
-    <S.Button variant={variant}>{children}</S.Button>
+/**
+ * @typedef {('primary'|'secondary'|'link'|'transparent'|'disabled')} ButtonVariant
+ * @param {Object} props - The component props.
+ * @param {Object} props.style - Style object
+ * @param {string} props.children - Text to be displayed inside the button
+ * @param {string} props.redirect - Path to redirect when the button is clicked
+ * @param {ButtonVariant} props.variant - Button variant
+ * @param {string} props.type - Button type
+ * @param {(e: MouseEvent<HTMLButtonElement, MouseEvent>) => void} props.onClick - Function to be executed when the button is clicked
+ * @param {boolean} props.disabled - If the button is disabled
+ * @param {boolean} props.full - If the button should be full width
+ * @returns {JSX.Element}
+ */
+const Button = ({ type, variant, children, onClick, redirect, full, disabled }) => {
+  return redirect ? (
+    <S.LinkButton to={redirect} variant={variant} onClick={onClick} full={full} disabled={disabled}>
+      {children}
+    </S.LinkButton>
+  ) : (
+    <S.Button type={type} variant={disabled ? "disabled" : variant} onClick={onClick} full={full} disabled={disabled}>{children}</S.Button>
   )
 }
 
 export default Button
 
 Button.propTypes = {
-    variant: PropTypes.oneOf(['primary', 'secondary', 'transparent']),
-    children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  variant: PropTypes.oneOf(['primary', 'secondary', 'transparent', 'link', 'disabled']),
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  redirect: PropTypes.string,
+  full: PropTypes.bool,
+  disabled: PropTypes.bool,
 }
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,6 +1,6 @@
 import { styled, css } from 'styled-components'
+import { Link } from 'react-router-dom';
 
-// apply directly styles for each variant
 const getVariant = (theme, variant) => {
     switch (variant) {
         case "primary":
@@ -9,10 +9,18 @@ const getVariant = (theme, variant) => {
                 color: ${theme.button.primary.text};
                 border: 1px solid ${theme.button.primary.border};
 
+                & > * {
+                    color: ${theme.button.primary.text};
+                }
+
                 &:hover {
                     background-color: ${theme.button.primary.hover.background};
                     color: ${theme.button.primary.hover.text};
                     border: 1px solid ${theme.button.primary.hover.border};
+
+                    & > * {
+                        color: ${theme.button.primary.hover.text};
+                    }
                 }
             `;
         case "secondary":
@@ -21,10 +29,18 @@ const getVariant = (theme, variant) => {
                 color: ${theme.button.secondary.text};
                 border: 1px solid ${theme.button.secondary.border};
 
+                & > * {
+                    color: ${theme.button.secondary.text};
+                }
+
                 &:hover {
                     background-color: ${theme.button.secondary.hover.background};
                     color: ${theme.button.secondary.hover.text};
                     border: 1px solid ${theme.button.secondary.hover.border};
+
+                    & > * {
+                        color: ${theme.button.secondary.hover.text};
+                    }
                 }
             `;
         case "transparent":
@@ -33,10 +49,40 @@ const getVariant = (theme, variant) => {
                 color: ${theme.button.transparent.text};
                 border: 1px solid ${theme.button.transparent.border};
 
+                & > * {
+                    color: ${theme.button.transparent.text};
+                }
+
                 &:hover {
                     background-color: ${theme.button.transparent.hover.background};
                     color: ${theme.button.transparent.hover.text};
                     border: 1px solid ${theme.button.transparent.hover.border};
+                    opacity: 0.7;
+
+                    & > * {
+                        color: ${theme.button.transparent.hover.text};
+                    }
+                }
+            `;
+        case "link":
+            return css`
+                background-color: ${theme.button.link.background};
+                color: ${theme.button.link.text};
+                border: 1px solid ${theme.button.link.border};
+
+                & > * {
+                    color: ${theme.button.link.text};
+                }
+
+                &:hover {
+                    background-color: ${theme.button.link.hover.background};
+                    color: ${theme.button.link.hover.text};
+                    border: 1px solid ${theme.button.link.hover.border};
+                    opacity: 0.7;
+
+                    & > * {
+                        color: ${theme.button.link.hover.text};
+                    }
                 }
             `;
     }
@@ -48,5 +94,21 @@ export const Button = styled.button`
     border-radius: 8px;
     border: none;
     transition: all 0.3s;
+    font-size: 14px;
+    text-align: center;
+    width: ${({ full }) => (full ? "100%" : "auto")};
     ${({ variant, theme }) => getVariant(theme, variant || "primary")}
-`
+`;
+
+export const LinkButton = styled(Link)`
+	cursor: pointer;
+    padding: 8px 16px;
+    border-radius: 8px;
+    border: none;
+    transition: all 0.3s;
+    text-decoration: none;
+    font-size: 14px;
+    text-align: center;
+    width: ${({ full }) => (full ? "100%" : "auto")};
+    ${({ variant, theme }) => getVariant(theme, variant || "primary")}
+`;
