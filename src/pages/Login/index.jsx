0f8fa7ad2097e8e070a7678d6b7a59f6cf95095c
marcosjunior00 <marcosantonio.jose01@gmail.com>
2025-06-01 20:13:20 -0300

    feat: starting to insert hook form on login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,32 @@
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import * as S from "./styles";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useState } from "react";
+
+const schema = yup.object().shape({
+    email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+    password: yup.string().min(6, "Senha deve ter pelo menos 6 caracteres").required("Senha é obrigatória"),
+});
 
 const Login = () => {
+    const [loading, setLoading] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+        setLoading(true);
+        // Aqui você pode adicionar a lógica para autenticação do usuário
+    }
+
     return (
         <S.Container>
             <S.GlobalStyle />
@@ -14,12 +38,12 @@ const Login = () => {
             <S.Left>
                 <S.FormTitle>Login</S.FormTitle>
 
-                <S.Form>
-                    <Input label="E-mail:" placeholder="Digite o e-mail" type="text" />
-                    <Input label="Senha:" placeholder="Digite a senha" type="password" />
+                <S.Form onSubmit={handleSubmit(onSubmit)}>
+                    <Input label="E-mail:" placeholder="Digite o e-mail" type="text" register={register("email")} errors={errors.email} />
+                    <Input label="Senha:" placeholder="Digite a senha" type="password" register={register("password")} errors={errors.password} />
 
                     <Button variant="transparent" redirect="/register">Cadastro</Button>
-                    <Button variant="primary">Enviar</Button>
+                    <Button variant="primary" type="submit" disabled={loading}>Enviar</Button>
                 </S.Form>
             </S.Left>
 
