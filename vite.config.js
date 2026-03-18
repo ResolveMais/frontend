c7880322ec5a399254f307900881022742621f2d
marcosjunior00 <marcosantonio.jose01@gmail.com>
2026-03-18 10:54:36 -0300

    feat: enhance Vite configuration to load environment variables for dynamic port setting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = Number(env.VITE_PORT) || 3000
+
+  console.log(`Starting development server on port ${port}...`)
+
+  return {
+    plugins: [react()],
+    server: {
+      port,
+    },
+  }
 })
